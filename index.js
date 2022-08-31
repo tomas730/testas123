cb63e1d535ebbd17034c59d59b9eb2e85ae7e767
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-08-31 18:06:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
 const console = require('console')
-const fs = require('fs')
-
-// const path = './file.txt'
-// fs.unlink(path, (err) => {
-//   if (err) {
-//     console.error(err)
-//     return
-//   }
-// })
-
-test()
-async function test() {
-
-  // let myObj = { name: "Chris", age: 38 };
-// console.log(myObj)
-// let myString = JSON.parse(myObj);
-// console.log(myString)
-
-}
+console.log("test")
