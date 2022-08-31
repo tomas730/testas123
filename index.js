01c1b003f5157bd4f4b478f7a1c91877b5315a4c
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-08-31 18:05:11 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-test
+const console = require('console')
+const fs = require('fs')
+
+// const path = './file.txt'
+// fs.unlink(path, (err) => {
+//   if (err) {
+//     console.error(err)
+//     return
+//   }
+// })
+
+test()
+async function test() {
+
+  // let myObj = { name: "Chris", age: 38 };
+// console.log(myObj)
+// let myString = JSON.parse(myObj);
+// console.log(myString)
+
+}
