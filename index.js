7e9a9376c4217db3fc9a7630c3b880879c6b07fc
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-08-31 18:18:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const console = require('console')
-console.log("test")
+const express = require('express')
+const app = express();
+const bodyParser = require("body-parser")
+app.use(express.static(__dirname + '/views'));
+
+app.set('view-engine', 'ejs')
+
+app.get("/", async function(req, res) {
+
+//res.render('home.ejs')
+res.sendFile(__dirname + "./views/index.html")
+// res.redirect("/")
+})
+
+app.listen(443, function() {
+    console.log("server on 443 port")
+})
