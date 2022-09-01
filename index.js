ae795b75adcb0986ffb28cc859a340e2bf40d159
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-09-01 18:52:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express();
 
 app.get("/", async function(req, res) {
-//res.send("te")
+res.send("te")
 //res.render('home.ejs')
-res.sendFile(dirname + "./index.html")
+//res.sendFile(dirname + "./index.html")
 // res.redirect("/")
 })
 
-app.listen(3000, function() {
-    console.log("server on 3000 port")
+app.listen(80, function() {
+    console.log("server on 80 port")
 })
