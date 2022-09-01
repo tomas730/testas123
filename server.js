68d93d76804e7fd76307beb302c2294e373d44c9
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-09-01 19:47:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,34 +6,15 @@ const ytdl = require('ytdl-core');
 const youtube = require("random-youtube-video-by-keyword")
 const path = require("path")
 
-
 app.use(express.static(__dirname + '/views'));
 app.set('view-engine', 'ejs')
 
-
 mongoose.connect(mongoose.env.MONGODB)
-const dbSchema = {
-    id: {
-        type: String,
-        require: true,
-    },
-    url: {
-        type: String,
-        require: true,
-    }
-}
-const db = mongoose.model("id", dbSchema)
+
 
   app.post('/', async (req, res) => {
-      const b = await db.findOne({id:`${req.body.upvote}`})
-      if(!b || b === "null") {
-          res.send("found")
-      }
-    console.log(req.body.upvote)
-    console.log(req.body.option)
       res.sendFile(__dirname + "/views/error.html")
   })
-//test
 
 app.get("/", async function(req, res) {
 //res.sendFile(__dirname + "/views/index.html")
