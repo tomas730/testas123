f484406f6b82d3f4616fb475bafa9036c290f688
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-09-01 18:41:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 console.log("thru")
 const express = require('express')
 const app = express();
-const bodyParser = require("body-parser")
-app.use(express.static(dirname + '/views'));
 
 app.get("/", async function(req, res) {
 res.send("te")
 //res.render('home.ejs')
-//res.sendFile(dirname + "./views/index.html")
+res.sendFile(dirname + "./index.html")
 // res.redirect("/")
 })
 
