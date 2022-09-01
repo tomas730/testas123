73b32e442bd4559f329e36d693836423be4eccdd
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-09-01 19:16:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express();
+const mongoose = require("mongoose");
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const youtube = require("random-youtube-video-by-keyword")
+const path = require("path")
+const key = process.env.key
+
+app.use(express.static(__dirname + '/views'));
+
+app.set('view-engine', 'ejs')
 
 app.get("/", async function(req, res) {
-res.sendFile(__dirname + "/views/index.html")
+//res.sendFile(__dirname + "/views/index.html")
+res.render("index.ejs")
 })
 
 app.listen(3000, function() {
