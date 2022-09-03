1c2a7b6172689470d7f630b956b1a977707b77c0
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-09-03 13:58:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,11 @@ const mongoose = require("mongoose");
 
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const youtube = require("random-youtube-video-by-keyword")
-const path = require("path")
 const console = require('console');
 
 app.use(express.static(__dirname + '/views'));
 app.set('view-engine', 'ejs')
-
   
-} catch (e) {
-  console.log(e)
-            }
   app.post('/', async (req, res) => {
   
    mongoose.connect("mongodb+srv://tomas123:PornstaR$730@cluster0.o0ntvjx.mongodb.net/Discorddb?retryWrites=true&w=majority")
@@ -41,7 +35,7 @@ const db = mongoose.model("id", dbSchema)
 const b = await db.findOne({id:`v4`})
   res.send(b.url+ " here")
     var stream = ytdl(b.url,{ quality: 'lowestaudio', format: 'webm'});
-    stream.pipe(fs.createWriteStream('./views/downloads/' + "num" + '.mp3'))
+    stream.pipe(fs.createWriteStream('./views/' + "num" + '.mp3'))
   })
 
 app.get("/", async function(req, res) {
