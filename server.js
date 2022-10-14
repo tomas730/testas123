bcfb847098159a53e8b519421dd1931d720b2a5b
tomas730 <88490658+tomas730@users.noreply.github.com>
2022-10-14 18:39:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,29 +11,30 @@ app.use(express.static(__dirname + '/views'));
 app.set('view-engine', 'ejs')
   
   app.post('/', async (req, res) => {
-  console.log(process.env.mongodb_URI)
-   mongoose.connect(process.env.mongodb_URI)
-  .then(() => {
-    console.info("Connected to the database");
-  })
-  .catch(err => {
-    console.info("Cannot connect to the database!", err);
-    process.exit();
-  });
+    res.send("pressed")
+//   console.log(process.env.mongodb_URI)
+//    mongoose.connect(process.env.mongodb_URI)
+//   .then(() => {
+//     console.info("Connected to the database");
+//   })
+//   .catch(err => {
+//     console.info("Cannot connect to the database!", err);
+//     process.exit();
+//   });
     
-  const dbSchema = {
-    id: {
-        type: String,
-        require: true,
-    },
-    url: {
-        type: String,
-        require: true,
-    }
-}
-const db = mongoose.model("id", dbSchema)
-const b = await db.findOne({id:`v4`})
-  res.send(b.url+ " here")
+//   const dbSchema = {
+//     id: {
+//         type: String,
+//         require: true,
+//     },
+//     url: {
+//         type: String,
+//         require: true,
+//     }
+// }
+// const db = mongoose.model("id", dbSchema)
+// const b = await db.findOne({id:`v4`})
+//   res.send(b.url+ " here")
 //     var stream = ytdl(b.url,{ quality: 'lowestaudio', format: 'webm'});
 //     stream.pipe(fs.createWriteStream('./views/' + "num" + '.mp3'))
   })
